5f39c00da1d3d843a20aeacc4cf3e51ac006291a
MelissaHalo <melissateachesall43@gmail.com>
2026-05-19 14:25:17 -0400

    Implement solve() function
    Replaced placeholder code with working logic to return x * x.

--- tasks/square_number/starter_solution.py
+++ tasks/square_number/starter_solution.py
@@ -1,7 +1,4 @@
 def solve(x: int) -> int:
-    """
-    Return the square of x.
-    Students must implement this function.
-    """
-    # TODO: implement the correct logic
-    raise NotImplementedError("You must implement the solve() function.")
+ return x * x
+
+
